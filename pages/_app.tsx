28f8e98e65b1954bfe7c80d63480daaec50ef46b
PiotrRut <piotr.rutkowski@city.ac.uk>
2021-11-13 23:44:14 +0000

    feat(config): Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '@theme/globals.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
