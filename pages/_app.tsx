c0313faa277fab816b1bdc8fc470ff9c9c5f0611
PiotrRut <piotr.rutkowski@city.ac.uk>
2021-11-14 21:27:20 +0000

    feat(deps): Install axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '@theme/globals.css';
 
+import Axios from 'axios';
+import useAxios, { configure } from 'axios-hooks';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+
+const axios = Axios.create({
+  baseURL: 'https://reqres.in/api',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    configure({ axios });
+  }, []);
+
+  const [{ data, loading, error }] = useAxios('/hello');
+
+  useEffect(() => {
+    console.log(data, loading, error);
+  }, [data, loading, error]);
+
   return <Component {...pageProps} />;
 }
 
