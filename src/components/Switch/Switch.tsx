4118473108eddaed3a2616748037877c7cfc16a0
PiotrRut <piotr.rutkowski@city.ac.uk>
2022-01-05 20:52:59 +0100

    feat(multi): Added filtering to to-dos :sparkles:

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,6 +3,11 @@ import React, { FunctionComponent } from 'react';
 import { SwitchProps } from './Switch.models';
 import { StyledSwitch, StyledThumb } from './Switch.styles';
 
+/**
+ * A switch/toggle dialog. Bases it's core functionality on the Radix primitive switch.
+ *
+ * Ref - https://www.radix-ui.com/docs/primitives/components/switch
+ */
 const Switch: FunctionComponent<SwitchProps> = (props) => {
   const { name, ...rest } = props;
 
